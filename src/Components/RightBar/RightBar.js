b076554a68e22cff17e3b2e330abcf6d04731124
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-11-01 12:40:35 -0400

    Adding Scroll Div to Right Bar

--- src/Components/RightBar/RightBar.js
+++ src/Components/RightBar/RightBar.js
@@ -11,6 +11,35 @@ const Block = styled.div`
     top: 0;
 `;
 
-const RightBar = () => <Block />;
+const ScrollCont = styled.div`
+    transform: rotate(90deg);
+    position: absolute;
+    right: -5%;
+    bottom: 250px;
+    height: 30px;
+    color: white;
+
+    p {
+        display: inline-block;
+        padding: 0;
+    }
+`;
+
+const Line = styled.div`
+    width: 150px;
+    height: 1px;
+    background-color: white;
+    display: inline-block;
+    margin: 0 30px 3px 0;
+`;
+
+const RightBar = () => (
+    <Block>
+        <ScrollCont>
+            <Line />
+            <p>Scroll Down</p>
+        </ScrollCont>
+    </Block>
+);
 
 export default RightBar;
