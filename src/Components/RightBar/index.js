18456dff76147678e46b4e9e1c53a7f6857d607e
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-11-06 00:07:44 -0500

    Resctructuring to match architecture

--- src/Components/RightBar/index.js
+++ src/Components/RightBar/index.js
@@ -1,1 +1,57 @@
-export { default } from './RightBar';
+import React from 'react';
+import { Block, ScrollCont, Line } from './styles';
+
+export default class RightBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            minimize: false
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', () => this.handleScroll());
+    }
+
+    handleScroll() {
+        if (window.pageYOffset >= 500) {
+            this.setState({
+                minimize: true
+            });
+        } else {
+            this.setState({
+                minimize: false
+            });
+        }
+    }
+
+    render() {
+        const { minimize } = this.state;
+        return (
+            <Block
+                style={
+                    minimize
+                        ? {
+                              transitionDuration: '.5s',
+                              transform: 'translate(90%,0)'
+                          }
+                        : {}
+                }
+            >
+                <ScrollCont
+                    style={
+                        minimize
+                            ? {
+                                  transition: 'opacity  .3s',
+                                  opacity: '0'
+                              }
+                            : { opacity: '1' }
+                    }
+                >
+                    <Line />
+                    <p>Scroll Down</p>
+                </ScrollCont>
+            </Block>
+        );
+    }
+}
---
+++ src/Components/RightBar/styles.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { COLORS, QUERIES } from '../../Styling';
+
+export const Block = styled.div`
+    width: 10%;
+    height: 100vh;
+    background-color: ${COLORS.deepBlue};
+    position: fixed;
+    right: 0;
+    top: 0;
+    transform: none;
+    transition: transform 0.5s;
+    display: none;
+
+    @media (${QUERIES.medium}) {
+        width: 30%;
+        display: block;
+    }
+`;
+
+export const ScrollCont = styled.div`
+    transform: rotate(90deg);
+    position: absolute;
+    right: -5%;
+    bottom: 250px;
+    height: 30px;
+    color: white;
+    display: none;
+    transition: opacity 0.5s 0.3s;
+
+    p {
+        display: inline-block;
+        padding: 0;
+    }
+
+    @media (${QUERIES.medium}) {
+        display: block;
+    }
+`;
+
+export const Line = styled.div`
+    width: 150px;
+    height: 1px;
+    background-color: white;
+    display: inline-block;
+    margin: 0 30px 3px 0;
+`;
