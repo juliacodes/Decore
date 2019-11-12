cd8c9fc7b0364de30e7c93511c2206944aaa7ea1
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-11-12 13:13:25 -0500

    Added mapping for builder

--- src/Components/editor/index.js
+++ src/Components/editor/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import EditorWrapper from './styles';
+import { EditorWrapper, BuildChild, SplitDiv, ChildDiv } from './styles';
+import buildData from '../../store/buildData';
 
 export default class Editor extends React.Component {
     constructor() {
         super();
 
-        this.state = {};
+        this.state = {
+            builds: buildData.builds,
+            // eslint-disable-next-line react/no-unused-state
+            build: buildData.builds[0]
+        };
     }
 
     render() {
+        const { builds } = this.state;
         return (
             <EditorWrapper>
                 <p>Editor</p>
+                {builds.map(build => {
+                    // eslint-disable-next-line react/destructuring-assignment
+                    if (build.type === 'split-div') {
+                        return (
+                            <BuildChild>
+                                <SplitDiv>{build.type}</SplitDiv>
+                                <SplitDiv>{build.type}</SplitDiv>
+                            </BuildChild>
+                        );
+                    }
+                    return (
+                        <BuildChild>
+                            <ChildDiv>{build.type}</ChildDiv>
+                        </BuildChild>
+                    );
+                })}
             </EditorWrapper>
         );
     }
