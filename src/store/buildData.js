0b95e33f46ebbaba16be5881167e3f1fb26ab62a
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-11-12 13:13:13 -0500

    Added buildData object

---
+++ src/store/buildData.js
@@ -0,0 +1,18 @@
+const buildData = {
+    builds: [
+        {
+            index: 0,
+            type: 'div'
+        },
+        {
+            index: 1,
+            type: 'div'
+        },
+        {
+            index: 2,
+            type: 'split-div'
+        }
+    ]
+};
+
+export default buildData;
