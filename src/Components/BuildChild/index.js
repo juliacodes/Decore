1af102e9797cac9ced3fbe8039e4f77abd70cd31
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-12-01 23:29:50 -0500

    Changed to Class

--- src/Components/BuildChild/index.js
+++ src/Components/BuildChild/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import {
     BuildChildCont,
@@ -7,33 +8,64 @@ import {
     TriDiv
 } from '../editor/styles';
 
-function BuildChild(props) {
-    const { name } = props;
-    return (
-        <BuildChildCont>
-            {name === 'split-div' && (
-                <>
-                    <SplitDiv>{name}</SplitDiv>
-                    <SplitDiv>{name}</SplitDiv>
-                </>
-            )}
+// function BuildChild(props) {
+//   const { name } = props;
+//   return (
+//     <BuildChildCont>
+//       {name === "split-div" && (
+//         <>
+//           <SplitDiv>{name}</SplitDiv>
+//           <SplitDiv>{name}</SplitDiv>
+//         </>
+//       )}
 
-            {name === 'nav' && (
-                <>
-                    <NavDiv>{name}</NavDiv>
-                </>
-            )}
+//       {name === "nav" && (
+//         <>
+//           <NavDiv>{name}</NavDiv>
+//         </>
+//       )}
 
-            {name === 'tri-div' && (
-                <>
-                    <TriDiv>{name}</TriDiv>
-                    <TriDiv>{name}</TriDiv>
-                    <TriDiv>{name}</TriDiv>
-                </>
-            )}
-            {name === 'div' && <ChildDiv>{name}</ChildDiv>}
-        </BuildChildCont>
-    );
+//       {name === "tri-div" && (
+//         <>
+//           <TriDiv>{name}</TriDiv>
+//           <TriDiv>{name}</TriDiv>
+//           <TriDiv>{name}</TriDiv>
+//         </>
+//       )}
+//       {name === "div" && <ChildDiv>{name}</ChildDiv>}
+//     </BuildChildCont>
+//   );
+// }
+
+class BuildChild extends React.Component {
+    render() {
+        const { name } = this.props;
+        return (
+            <BuildChildCont>
+                {name === 'split-div' && (
+                    <>
+                        <SplitDiv>{name}</SplitDiv>
+                        <SplitDiv>{name}</SplitDiv>
+                    </>
+                )}
+
+                {name === 'nav' && (
+                    <>
+                        <NavDiv>{name}</NavDiv>
+                    </>
+                )}
+
+                {name === 'tri-div' && (
+                    <>
+                        <TriDiv>{name}</TriDiv>
+                        <TriDiv>{name}</TriDiv>
+                        <TriDiv>{name}</TriDiv>
+                    </>
+                )}
+                {name === 'div' && <ChildDiv>{name}</ChildDiv>}
+            </BuildChildCont>
+        );
+    }
 }
 
 export default BuildChild;
