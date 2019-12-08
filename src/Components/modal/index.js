499e0e7af7dacf5396e4f6cfdb5d57096b03e263
Ian Trent <ian.m.trent@gmail.com>
2019-12-08 12:35:12 -0500

    Added favicon

--- src/Components/modal/index.js
+++ src/Components/modal/index.js
@@ -1,85 +1,85 @@
-import React from "react";
+import React from 'react';
 import {
-  ModalBackground,
-  ModalWrapper,
-  CodeModalWrapper,
-  ModalHeading,
-  ModalBody
-} from "./styles";
+    ModalBackground,
+    ModalWrapper,
+    CodeModalWrapper,
+    ModalHeading,
+    ModalBody
+} from './styles';
 
 export default class Modal extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      active: false
+        this.state = {
+            active: false
+        };
+
+        this.modal = React.createRef();
+    }
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.handleClickOutside);
+    }
+
+    toggleModal = status => {
+        this.setState({ active: status });
     };
 
-    this.modal = React.createRef();
-  }
+    handleClickOutside = e => {
+        if (this.modal.current && !this.modal.current.contains(e.target)) {
+            this.setState({ active: false });
+        }
+    };
 
-  componentDidMount() {
-    document.addEventListener("mousedown", this.handleClickOutside);
-  }
+    render() {
+        const { active } = this.state;
+        const { title, children, modalType } = this.props;
 
-  toggleModal = status => {
-    this.setState({ active: status });
-  };
-
-  handleClickOutside = e => {
-    if (this.modal.current && !this.modal.current.contains(e.target)) {
-      this.setState({ active: false });
+        return (
+            <ModalBackground className={active && 'active'}>
+                {modalType === 'codeModal' ? (
+                    <CodeModalWrapper ref={this.modal}>
+                        <ModalHeading>
+                            <p>{title}</p>
+                            <svg
+                                width="18"
+                                height="18"
+                                viewBox="0 0 18 18"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                onClick={() => this.toggleModal(false)}
+                            >
+                                <path
+                                    d="M12.0938 9L16.7812 4.3125C17.3906 3.75 17.3906 2.8125 16.7812 2.25L15.75 1.21875C15.1875 0.609375 14.25 0.609375 13.6875 1.21875L9 5.90625L4.26562 1.21875C3.70312 0.609375 2.76562 0.609375 2.20312 1.21875L1.17188 2.25C0.5625 2.8125 0.5625 3.75 1.17188 4.3125L5.85938 9L1.17188 13.7344C0.5625 14.2969 0.5625 15.2344 1.17188 15.7969L2.20312 16.8281C2.76562 17.4375 3.70312 17.4375 4.26562 16.8281L9 12.1406L13.6875 16.8281C14.25 17.4375 15.1875 17.4375 15.75 16.8281L16.7812 15.7969C17.3906 15.2344 17.3906 14.2969 16.7812 13.7344L12.0938 9Z"
+                                    fill="#8F8F8F"
+                                />
+                            </svg>
+                        </ModalHeading>
+                        <ModalBody>{children}</ModalBody>
+                    </CodeModalWrapper>
+                ) : (
+                    <ModalWrapper ref={this.modal}>
+                        <ModalHeading>
+                            <p>{title}</p>
+                            <svg
+                                width="18"
+                                height="18"
+                                viewBox="0 0 18 18"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                onClick={() => this.toggleModal(false)}
+                            >
+                                <path
+                                    d="M12.0938 9L16.7812 4.3125C17.3906 3.75 17.3906 2.8125 16.7812 2.25L15.75 1.21875C15.1875 0.609375 14.25 0.609375 13.6875 1.21875L9 5.90625L4.26562 1.21875C3.70312 0.609375 2.76562 0.609375 2.20312 1.21875L1.17188 2.25C0.5625 2.8125 0.5625 3.75 1.17188 4.3125L5.85938 9L1.17188 13.7344C0.5625 14.2969 0.5625 15.2344 1.17188 15.7969L2.20312 16.8281C2.76562 17.4375 3.70312 17.4375 4.26562 16.8281L9 12.1406L13.6875 16.8281C14.25 17.4375 15.1875 17.4375 15.75 16.8281L16.7812 15.7969C17.3906 15.2344 17.3906 14.2969 16.7812 13.7344L12.0938 9Z"
+                                    fill="#8F8F8F"
+                                />
+                            </svg>
+                        </ModalHeading>
+                        <ModalBody>{children}</ModalBody>
+                    </ModalWrapper>
+                )}
+            </ModalBackground>
+        );
     }
-  };
-
-  render() {
-    const { active } = this.state;
-    const { title, children, modalType } = this.props;
-
-    return (
-      <ModalBackground className={active && "active"}>
-        {modalType === "codeModal" ? (
-          <CodeModalWrapper ref={this.modal}>
-            <ModalHeading>
-              <p>{title}</p>
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                onClick={() => this.toggleModal(false)}
-              >
-                <path
-                  d="M12.0938 9L16.7812 4.3125C17.3906 3.75 17.3906 2.8125 16.7812 2.25L15.75 1.21875C15.1875 0.609375 14.25 0.609375 13.6875 1.21875L9 5.90625L4.26562 1.21875C3.70312 0.609375 2.76562 0.609375 2.20312 1.21875L1.17188 2.25C0.5625 2.8125 0.5625 3.75 1.17188 4.3125L5.85938 9L1.17188 13.7344C0.5625 14.2969 0.5625 15.2344 1.17188 15.7969L2.20312 16.8281C2.76562 17.4375 3.70312 17.4375 4.26562 16.8281L9 12.1406L13.6875 16.8281C14.25 17.4375 15.1875 17.4375 15.75 16.8281L16.7812 15.7969C17.3906 15.2344 17.3906 14.2969 16.7812 13.7344L12.0938 9Z"
-                  fill="#8F8F8F"
-                />
-              </svg>
-            </ModalHeading>
-            <ModalBody>{children}</ModalBody>
-          </CodeModalWrapper>
-        ) : (
-          <ModalWrapper ref={this.modal}>
-            <ModalHeading>
-              <p>{title}</p>
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                onClick={() => this.toggleModal(false)}
-              >
-                <path
-                  d="M12.0938 9L16.7812 4.3125C17.3906 3.75 17.3906 2.8125 16.7812 2.25L15.75 1.21875C15.1875 0.609375 14.25 0.609375 13.6875 1.21875L9 5.90625L4.26562 1.21875C3.70312 0.609375 2.76562 0.609375 2.20312 1.21875L1.17188 2.25C0.5625 2.8125 0.5625 3.75 1.17188 4.3125L5.85938 9L1.17188 13.7344C0.5625 14.2969 0.5625 15.2344 1.17188 15.7969L2.20312 16.8281C2.76562 17.4375 3.70312 17.4375 4.26562 16.8281L9 12.1406L13.6875 16.8281C14.25 17.4375 15.1875 17.4375 15.75 16.8281L16.7812 15.7969C17.3906 15.2344 17.3906 14.2969 16.7812 13.7344L12.0938 9Z"
-                  fill="#8F8F8F"
-                />
-              </svg>
-            </ModalHeading>
-            <ModalBody>{children}</ModalBody>
-          </ModalWrapper>
-        )}
-      </ModalBackground>
-    );
-  }
 }
--- src/Components/modal/styles.js
+++ src/Components/modal/styles.js
@@ -1,76 +1,78 @@
-import styled from "styled-components";
-import { COLORS, FONTS } from "../../Styling/Styling";
+import styled from 'styled-components';
+import { COLORS, FONTS } from '../../Styling/Styling';
 
 export const ModalWrapper = styled.div`
-  background-color: white;
-  transform: translateY(100px);
-  max-width: 1000px;
-  width: 750px;
-  margin: 0 20px;
-  transition: 0.3s ease;
-  box-shadow: 0px 0px 100px rgba(0, 0, 0, 0.23);
-  border-radius: 10px;
+    position: relative;
+    background-color: white;
+    transform: translateY(100px);
+    max-width: 1000px;
+    width: 750px;
+    margin: 0 20px;
+    transition: 0.3s ease;
+    box-shadow: 0px 0px 100px rgba(0, 0, 0, 0.23);
+    border-radius: 10px;
+    z-index: 2;
 `;
 
 export const CodeModalWrapper = styled.div`
-  background-color: white;
-  transform: translateY(0px);
-  max-width: 1200px;
-  width: 1200px;
-  margin: 0 20px;
-  transition: 0.3s ease;
-  box-shadow: 0px 0px 100px rgba(0, 0, 0, 0.23);
-  border-radius: 10px;
+    background-color: white;
+    transform: translateY(0px);
+    max-width: 1200px;
+    width: 1200px;
+    margin: 0 20px;
+    transition: 0.3s ease;
+    box-shadow: 0px 0px 100px rgba(0, 0, 0, 0.23);
+    border-radius: 10px;
 `;
 
 export const ModalBackground = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0.4);
-  z-index: 1;
-  visibility: hidden;
-  pointer-events: none;
-  opacity: 0;
-  transition: 0.3s ease;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+    z-index: 1;
+    visibility: hidden;
+    pointer-events: none;
+    opacity: 0;
+    transition: 0.3s ease;
 
-  &.active {
-    visibility: visible;
-    pointer-events: all;
-    opacity: 1;
+    &.active {
+        visibility: visible;
+        pointer-events: all;
+        opacity: 1;
 
-    ${ModalWrapper} {
-      transform: translateY(0);
+        ${ModalWrapper} {
+            transform: translateY(0);
+        }
     }
-  }
 `;
 
 export const ModalHeading = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  padding: 20px;
-  box-sizing: border-box;
-  box-shadow: 0px 1px 5px rgba(208, 208, 208, 0.5);
-  font-family: ${FONTS.secondary};
-  p {
-    margin: 0;
-    font-size: 30px;
-    color: ${COLORS.slate};
-  }
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 20px;
+    box-sizing: border-box;
+    box-shadow: 0px 1px 5px rgba(208, 208, 208, 0.5);
+    font-family: ${FONTS.secondary};
+    p {
+        margin: 0;
+        font-size: 30px;
+        color: ${COLORS.slate};
+    }
 
-  svg {
-    cursor: pointer;
-  }
+    svg {
+        cursor: pointer;
+    }
 `;
 
 export const ModalBody = styled.div`
-  padding: 0 20px;
-  font-family: ${FONTS.secondary};
+    padding: 0 20px;
+    font-family: ${FONTS.secondary};
 `;
